c7350251f3e89edec71101d16682d6e6b328d41e
Amandine Bélien <amandine.belien@smartpanda.com>
2019-06-21 17:15:07 +0200

    feat(resultats): affichage des paths des fichiers créés

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const { app, BrowserWindow, dialog } = require('electron')
+const { app, BrowserWindow, dialog, ipcMain} = require('electron')
+const csv = require('fast-csv') 
+const path = require('path')
 
+const headersPlayers = ["numticket","nom","prenom","numtel"]
+const headersWinners = headersPlayers.concat("lot")
+
+let nbWinners = 0;
 // Gardez une reference globale de l'objet window, si vous ne le faites pas, la fenetre sera
 // fermee automatiquement quand l'objet JavaScript sera garbage collected.
 let win
@@ -21,73 +27,12 @@ function createWindow () {
   // Ouvre les DevTools.
   win.webContents.openDevTools()
 
-  let nbWInners;
-    $('#text-input').bind('input propertychange', function() {
-        nbWInners = this.value
-        console.log(nbWInners);
-    })
+    let nbWInners;
+      // $('#text-input').bind('input propertychange', function() {
+    //     nbWInners = this.value
+    //     console.log(nbWInners);
+    // })
 
-    $('#openFile').bind('click', function() {
-      let pathTombolaFile = dialog.showOpenDialog(
-        { 
-          properties: ['openFile'], 
-          filters: [
-            { name: 'Excel ou CSV', extensions: ['csv', 'xls'] }
-          ]
-        }
-      )
-  
-      let tickets = [];
-      let winners = [];
-      let firstLine=true;
-      csv
-        .parseFile(pathTombolaFile[0], {headers:true})
-        .on('error', error => console.error(error))
-        .on('data', row => {
-            tickets.push(row);
-            
-        })
-        .on('end', randomAndWriteFile => {
-          let i = 0;
-          while (i < numWinners) {
-            let winner = randomTicket(tickets);
-            winners.push(winner);
-            tickets = arrayRemoveNumticket(tickets, winner);
-            i++;
-          }
-  
-          csv.writeToPath(path.resolve(__dirname, 'tmp.csv'), winners.sort(compare), {headers: ["numticket","nom","prenom","numtel"]})
-          .on('error', err => console.error(err))
-          .on('finish', () => console.log('Done writing.'));
-        });
-  
-    })
-
-      
-    function randomTicket(tickets) {
-      return tickets[Math.floor(Math.random() * tickets.length)];
-    }
-
-    function arrayRemoveNumticket(arr, value) {
-      return arr.filter(function(ele){
-          return ele.num_ticket != value.num_ticket;
-      });
-    }
-
-    function compare(a, b) {
-      // Use toUpperCase() to ignore character casing
-      const nomA = a.nom.toUpperCase();
-      const nomB = b.nom.toUpperCase();
-
-      let comparison = 0;
-      if (nomA >= nomB) {
-        comparison = 1;
-      } else if (nomA <= nomB) {
-        comparison = -1;
-      }
-      return comparison;
-    }
-    
 
   // Émit lorsque la fenêtre est fermée.
   win.on('closed', () => {
@@ -96,6 +41,90 @@ function createWindow () {
     // où vous devez supprimer l'élément correspondant.
     win = null
   })
+}
+
+ipcMain.on('set-nb-winners', (event,arg) => {
+  nbWinners = arg;
+})
+
+ipcMain.on('click-button',(event,arg)=> {
+  if(arg=='true'){
+    dialog.showOpenDialog(
+      { 
+        properties: ['openFile'], 
+        filters: [
+          { name: 'Excel ou CSV', extensions: ['csv', 'xls'] }
+        ]
+      }, (files) =>{
+        callbackClick(files);
+      })   
+  }
+})
+
+function callbackClick(files) {
+  let tickets = [];
+  let winners = [];
+
+  csv.parseFile(files[0], { headers: headersPlayers, renameHeaders: true })
+    .on('error', error => console.error(error))
+    .on('data', row => {
+        tickets.push(row);
+    })
+    .on('end', randomAndWriteFile => {
+      let i = 1;
+      while (i <= nbWinners) {
+        let winner = randomTicket(tickets);
+        winner.lot = i;
+        winners.push(winner);
+        tickets = arrayRemoveNumticket(tickets, winner);
+        i++;
+      }
+
+      csv.writeToPath(path.resolve(__dirname, 'gagnants_par_alphabetique.csv'), winners.sort(compare), { headers: headersWinners})
+      .on('error', err => console.error(err))
+      .on('finish', () => win.webContents.send('gagnants_par_alphabetique', path.resolve(__dirname, 'gagnants_par_alphabetique.csv')));
+
+      csv.writeToPath(path.resolve(__dirname, 'gagnants_par_lot.csv'), winners.sort(compareLot), { headers: headersWinners})
+      .on('error', err => console.error(err))
+      .on('finish', () => win.webContents.send('gagnants_par_lot', path.resolve(__dirname, 'gagnants_par_lot.csv')));
+    });
+}
+
+function randomTicket(tickets) {
+  return tickets[Math.floor(Math.random() * tickets.length)];
+}
+
+function arrayRemoveNumticket(arr, value) {
+  return arr.filter(function(ele){
+      return ele.numticket != value.numticket;
+  });
+}
+
+function compare(a, b) {
+  // Use toUpperCase() to ignore character casing
+  const nomA = a.nom.toUpperCase();
+  const nomB = b.nom.toUpperCase();
+
+  let comparison = 0;
+  if (nomA >= nomB) {
+    comparison = 1;
+  } else if (nomA <= nomB) {
+    comparison = -1;
+  }
+  return comparison;
+}   
+
+function compareLot(a,b) {
+  const lotA = a.lot;
+  const lotB = b.lot;
+
+  let comparison = 0;
+  if (lotA >= lotB) {
+    comparison = 1;
+  } else if (lotA <= lotB) {
+    comparison = -1;
+  }
+  return comparison;
 }
 
 // Cette méthode sera appelée quant Electron aura fini
